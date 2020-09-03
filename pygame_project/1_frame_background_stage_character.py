6220bd2296b5e16e9d6627b901ece513584e376d
SeungWookHan <hswook12@me.com>
2020-09-04 01:26:45 +0900

    Add background, character, weapon, event, ball_movement etc..

--- pygame_project/1_frame_background_stage_character.py
+++ pygame_project/1_frame_background_stage_character.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 ####################################################################
 # 기본 초기화(무조건 해야함)
@@ -15,7 +16,24 @@ clock = pygame.time.Clock()
 ####################################################################
 
 # 사용자 게임 초기화(배경화면, 게임 이미지, 좌표, 폰트 등 설정)
+current_path = os.path.dirname(__file__)  # 현재 파일의 위치를 반환
+image_path = os.path.join(current_path, "images")
 
+# 배경 만들기
+background = pygame.image.load(os.path.join(image_path, "background.jpg"))
+
+# 스테이지 만들기
+# stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+# stage_size = stage.get_rect().size
+# stage_height = stage_size[1]
+
+# 캐릭터 만들기
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width - 2)
+character_y_pos = screen_height - character_height
 
 running = True
 while running:
@@ -31,6 +49,9 @@ while running:
     # 4. 충돌 처리
 
     # 5. 화면에 그리기
+    screen.blit(background, (0, 0))
+    # screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update()
 
